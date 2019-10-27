83466abe03293eb487d530938eb30bfdf01aad94
Adriano Laureano <sl4ureano@outlook.com>
2019-10-27 08:56:43 -0300

    [FIX] CORS Rules

--- api/src/app.js
+++ api/src/app.js
@@ -17,6 +17,11 @@ app.use(bodyParser.json());
 app.use('/', index);
 
 app.use(cors())
-app.options('*', cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
 
 module.exports = app;
