6558b63a912f30d643edb1f02454a56fc2835677
andremarzo <44248418+andremarzo@users.noreply.github.com>
2019-10-27 00:59:33 -0300

    Pag login com rotas.

--- prototipo/src/app/app.module.ts
+++ prototipo/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClient, HttpClientModule} from '@angular/common/http';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];  
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
    // FormsModule,
    // HttpClient,
    // HttpModule,
